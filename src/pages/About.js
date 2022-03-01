3c711e4b3e9c40865f658528a5e207c6cde45e3a
Sahil Gufran <sahilgufran2018@gmail.com>
2022-03-01 16:52:44 +0530

    1stcommit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MultiplePizzas from "../assets/multiplePizzas.jpeg";
+import "../styles/About.css";
+function About() {
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${MultiplePizzas})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1> ABOUT US</h1>
+        <p>
+         Here owner can write about the restaurant about how they make a pizza's and where they
+         are based, do they provide delivery services etc.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
